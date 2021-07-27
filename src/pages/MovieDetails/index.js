23e70ee144c8d3794dc609cd037481212d013690
Luis Maurício <36276548+cpluis@users.noreply.github.com>
2021-07-27 01:04:59 -0300

    Update index.js

--- src/pages/MovieDetails/index.js
+++ src/pages/MovieDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "../../components/Card";
 import GetDetailsMovie from "../../service/Movie/GetDetailsMovie";
